b8c1112f8b72ab03ba4fe02d8560589751298854
Franky Liang <fliangz96@gmail.com>
2017-06-13 14:12:12 -0400

    Add network communication with UDP multicast

---
+++ Pods/Target Support Files/Pods-raftUITests/Pods-raftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_raftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_raftUITests
+@end
--- raft/ViewController.swift
+++ raft/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import CocoaAsyncSocket
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GCDAsyncUdpSocketDelegate {
+    var udpSendSocket : GCDAsyncUdpSocket?
+    var udpReceiveSocket : GCDAsyncUdpSocket?
+    // multicast address range 224.0.0.0 to 239.255.255.255
+    let multicastIp = "225.1.2.3"
+    var sendQueue = DispatchQueue.init(label: "send")
+    var receiveQueue = DispatchQueue.init(label: "receive")
+    var sendTimer : Timer?
+    var receiveTimer : Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        udpSendSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: sendQueue)
+        udpReceiveSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: receiveQueue)
+        setupSockets()
+        sendTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(self.sendMulticast), userInfo: nil, repeats: true)
+    }
+    
+    func setupSockets() {
+        guard let sendSocket = udpSendSocket, let receiveSocket = udpReceiveSocket else {
+            print("Setup sockets")
+            return
+        }
+    
+        do {
+            try sendSocket.bind(toPort: 0)
+            try sendSocket.joinMulticastGroup(multicastIp)
+            try sendSocket.enableBroadcast(true)
+            try receiveSocket.bind(toPort: 2001)
+            try receiveSocket.joinMulticastGroup(multicastIp)
+            try receiveSocket.beginReceiving()
+        } catch {
+            print(error)
+        }
+    }
+
+    func sendMulticast() {
+        guard let socket = udpSendSocket, let dataStringz = "Local_IP".data(using: String.Encoding.utf8) else {
+            print("Stuff could not be initialized")
+            return
+        }
+        
+
+        socket.send(dataStringz, toHost: multicastIp, port: 2001, withTimeout: -1, tag: 0)
+
+    }
+
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        print(String(data: data, encoding: String.Encoding.utf8) ?? "Nothing")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
